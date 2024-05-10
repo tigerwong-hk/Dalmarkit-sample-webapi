f02395d91205be88014489d892ff76c841c9573d
tigerwong-hk <113076176+tigerwong-hk@users.noreply.github.com>
2024-05-10 10:53:51 +0800

    chore: update to Dalmarkit 0.2.2

--- src/Dalmarkit.Sample.EntityFrameworkCore/Entities/Entity.cs
+++ src/Dalmarkit.Sample.EntityFrameworkCore/Entities/Entity.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Dalmarkit.Sample.EntityFrameworkCore.Entities;
 
-public class Entity : PrincipalEntityBase
+public class Entity : PrincipalReadWriteEntityBase
 {
     [Key]
     [Required]

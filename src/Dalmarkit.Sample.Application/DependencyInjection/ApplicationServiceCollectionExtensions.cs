89534f09093d35b506ad9d805b1168711c4285eb
Tiger Wong <113076176+tigerwong-hk@users.noreply.github.com>
2026-05-03 21:09:41 +0800

    feat: kafka consumer

--- src/Dalmarkit.Sample.Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
+++ src/Dalmarkit.Sample.Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
@@ -1,11 +1,16 @@
 using Amazon.CloudFront;
 using Amazon.CognitoIdentityProvider;
 using Amazon.Extensions.NETCore.Setup;
+using Amazon.Kafka;
 using Amazon.Route53;
 using Amazon.S3;
 using Amazon.S3.Transfer;
 using Dalmarkit.Cloud.Aws.Services;
 using Dalmarkit.Common.Validation;
+using Dalmarkit.Messaging.Kafka.Config;
+using Dalmarkit.Messaging.Kafka.Consumers;
+using Dalmarkit.Messaging.Kafka.Handlers;
+using Dalmarkit.Sample.Application.PubSub.Kafka;
 using Dalmarkit.Sample.Application.Services.ApplicationServices;
 using Dalmarkit.Sample.Application.Services.DataServices;
 using Dalmarkit.Sample.Application.Services.ExternalServices;
@@ -18,6 +23,20 @@ public static class ApplicationServiceCollectionExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration config)
     {
+        _ = services.Configure<AwsKafkaOptions>(config.GetSection(AwsKafkaOptions.SectionName));
+        _ = services.AddOptions<KafkaConsumerOptions>()
+            .Bind(config.GetSection(KafkaConsumerOptions.SectionName))
+            .ValidateOnStart();
+
+        _ = services.AddSingleton<IBootstrapServersProvider, AwsSdkSaslIamBootstrapServersProvider>();
+        _ = services.AddSingleton<IOauthHandlers, AwsMskIamOauthHandlers>();
+        _ = services.AddSingleton<IKafkaMessageHandler<byte[]>, KafkaLoggingMessageHandler<byte[]>>();
+
+        // Single Byte-oriented Kafka consumer shared across every TPayload
+        // Envelope-aware deserialization happens in the handler
+        _ = services.AddSingleton<IKafkaConsumerService<byte[]>, KafkaConsumerService<byte[]>>();
+        _ = services.AddHostedService<KafkaConsumerBackgroundService<byte[]>>();
+
         _ = services.AddBlockchainServices();
         _ = services.AddCloudServices(config);
         _ = services.AddUtilityServices();
@@ -50,6 +69,9 @@ public static class ApplicationServiceCollectionExtensions
         _ = services.AddSingleton(_ => new AmazonCloudFrontClient(awsOptions.Region));
 
         _ = services.AddSingleton<IAmazonCognitoIdentityProvider>(_ => new AmazonCognitoIdentityProviderClient(awsOptions.Region));
+
+        _ = services.AddSingleton(_ => new AmazonKafkaClient(awsOptions.Region));
+        _ = services.AddSingleton<IAmazonKafka>(sp => sp.GetRequiredService<AmazonKafkaClient>());
 
         _ = services.AddSingleton(_ => new AmazonRoute53Client(awsOptions.Region));
 
